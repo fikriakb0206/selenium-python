d63ec96d8ebebc0a8196c6c4a20894219af3c746
fikriakb0206 <dekiakbar15@gmail.com>
2024-06-04 17:15:03 +0700

    update test cases contact

--- test_contact.py
+++ test_contact.py
@@ -15,20 +15,22 @@ Options.add_experimental_option("detach", True)
 
 @pytest.fixture
 def driver():
+# login
     driver = webdriver.Chrome(options=Options)
     driver.maximize_window()
     driver.get("https://trial.rpl.id/landing/index.html")
     home = driver.implicitly_wait(5)
     home = driver.find_element(By.CSS_SELECTOR, 'button.xbutton')
     home.click()
-    yield driver
-    
-def test_home(driver):    
     login = driver.find_element(By.XPATH, '//a[@href="/login"]')
     login.click()
     login = driver.find_element(By.XPATH, '//input[@name="email"]').send_keys ('rplkeysuper@gmail.com')
     login = driver.find_element(By.XPATH, '//input[@name="password"]').send_keys ('RPLsuper2023!')
     login = driver.find_element(By.XPATH, '//button[@type="submit"]')
     login.click()
+    yield driver
+    
+def test_login(driver):
+    contact = driver.find_element(By.XPATH, '//a[@href="/dashboard/sales-contact"]')
+    contact.click()
 
-# def login(driver):
